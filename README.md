d357472f9d1dc8167d1f3c2d9046b4b9146fbe46
Rafael Gonzalez <astrorafael@yahoo.es>
2019-08-02 20:31:28 +0200

    quitando cosas inutiles

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 4-channel square wave frequency generator based on ESP32's versatile RMT hardware module.
 
+![Console](doc/screenshot2.png?raw=true)
+
 Main characteristics:
 * Up to 4 independent channel outputs on GPIO pin #5,, #18 #19, #21
 * Frequency range from 0.01 Hz to 500 Khz
--- components/freq_console/freq_console.c
+++ components/freq_console/freq_console.c
@@ -1,15 +1,3 @@
-/* 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 /* ************************************************************************* */
 /*                         INCLUDE HEADER SECTION                            */
@@ -147,9 +135,3 @@ void freq_console_loop()
         linenoiseFree(line);
     }
 }
-
-#ifdef __cplusplus
-}
-#endif
-
-
